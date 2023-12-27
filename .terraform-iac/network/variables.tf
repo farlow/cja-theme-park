60c35215efa6bc7ed250744132fcbbb2883e3748
farlow <christophe.jacquemet@gmail.com>
2023-12-27 09:26:14 +0000

    terraform ok

--- .terraform-iac/network/variables.tf
+++ .terraform-iac/network/variables.tf
@@ -17,11 +17,3 @@ variable "private_subnets" {
 variable "public_subnets" {
   type = list(string)
 }
-
-variable "cloudns_auth_id" {
-  description = "Cloudns auth id"
-}
-
-variable "cloudns_password" {
-  description = "Cloudns password"
-}

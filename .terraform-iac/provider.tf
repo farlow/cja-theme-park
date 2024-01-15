00420a6197fbf4980dd3d6e9278f7ad88de1bb0c
farlow <christophe.jacquemet@gmail.com>
2024-01-15 11:24:43 +0000

    config backend S3 pour workflow infra-deploy

--- .terraform-iac/provider.tf
+++ .terraform-iac/provider.tf
@@ -1,10 +1,5 @@
 terraform {
   backend "s3" {
-    bucket =  "tprtf"
-    key = "tpriacstate/terraform.tfstate"
-#       region can't be set with variables.tf here, should be set with environment variable AWS_REGION
-    region = "eu-west-3"
-    dynamodb_table = "tpr-tfstate-locking"
     encrypt = true
   }
 
